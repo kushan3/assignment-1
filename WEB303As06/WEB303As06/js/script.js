5f61f088e6649b22ecbabe73cc011249e9ab106f
kushan3 <w0777326@myscc.ca>
2022-03-13 23:15:25 -0400

    Assignment-6

---
+++ WEB303As06/WEB303As06/js/script.js
@@ -0,0 +1,31 @@
+$(function () {
+    $('.accordion .accordion-control').on('click', function (e) {
+        e.preventDefault();
+
+        $(this)
+            .next('.accordion-panel')
+            .not(':animated')
+            // .stop()
+            .slideToggle();
+    });
+    $(".tab-list").each(function () {
+       
+        var $this = $(this); 
+        var $tab = $this.find("li.active"); 
+        var $link = $tab.find("a"); 
+        var $panel = $($link.attr("href")); 
+        $this.on("click", ".tab-control", function (e) {
+          
+          e.preventDefault(); 
+          var $link = $(this); 
+          var id = this.hash; 
+          if (id && !$link.parent().is(".active")) {
+        
+            $panel.removeClass("active"); 
+            $tab.removeClass("active"); 
+            $panel = $(id).addClass("active");
+            $tab = $link.parent().addClass("active");
+          }
+        });
+      });
+});
